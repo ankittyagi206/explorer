3b01cdfc9ea0173a42679e91d545418c675b7cb9
ankittyagi206 <61766976+ankittyagi206@users.noreply.github.com>
2023-06-25 22:34:55 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,8 @@
-import FilesFolder from "./components/FilesFolder";
 import Folder from "./components/Folder";
 import { explorer } from "./data/folderData";
 import { useState } from "react";
 import useTraverseTree from "./hooks/use-traverse-tree";
-const mockdata = [
-  {
-    id: "1",
-    foldername: "node_modules",
-    isExpended: true,
-    data: [
-      {
-        id: "5",
-        foldername: "NestedFolder",
-        isExpended: true,
-        isFolder: true,
-        data: [
-          {
-            isFolder: false,
-            fileName: "app.js",
-            id: "6",
-          },
-          {
-            isFolder: false,
-            fileName: "index.js",
-            id: "7",
-          },
-        ],
-      },
-      {
-        isFolder: false,
-        fileName: "app.js",
-        id: "6",
-      },
-      {
-        isFolder: false,
-        fileName: "index.js",
-        id: "7",
-      },
-    ],
-    isFolder: true,
-  },
-  {
-    id: "2",
-    fileName: "app.js",
-    isExpended: true,
-    isFolder: false,
-  },
-  {
-    id: "3",
-    foldername: "src",
-    isExpended: true,
-    data: [
-      {
-        isFolder: false,
-        id: "8",
-        fileName: "index.js",
-      },
-    ],
-    isFolder: true,
-  },
-  {
-    id: 4,
-    isFolder: false,
-    fileName: "package.json",
-  },
-];
+
 const App = () => {
   const { insertNode, deleteNode, updateNode } = useTraverseTree();
 
--- src/components/Folder.tsx
+++ src/components/Folder.tsx
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-
 const Folder = ({
   explorer,
   setExplorerData,
